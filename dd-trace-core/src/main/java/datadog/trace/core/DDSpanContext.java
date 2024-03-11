3b692ddde9f463e3afc763348af51e4df86f725d
Valentin Zakharov <valentin.zakharov1@gmail.com>
2024-03-12 00:55:38 +0100

    Introduced trace post-processing

--- dd-trace-core/src/main/java/datadog/trace/core/DDSpanContext.java
+++ dd-trace-core/src/main/java/datadog/trace/core/DDSpanContext.java
@@ -140,6 +140,7 @@ public class DDSpanContext
   private final boolean injectBaggageAsTags;
   private volatile int encodedOperationName;
   private volatile int encodedResourceName;
+  private boolean requiresPostProcessing;
 
   public DDSpanContext(
       final DDTraceId traceId,
@@ -944,5 +945,14 @@ public class DDSpanContext
   private String getTagName(String key) {
     // TODO is this decided?
     return "_dd." + key + ".json";
+  }
+
+  @Override
+  public void setRequiresPostProcessing(boolean postProcessing) {
+    this.requiresPostProcessing = postProcessing;
+  }
+
+  public boolean isRequiresPostProcessing() {
+    return requiresPostProcessing;
   }
 }
